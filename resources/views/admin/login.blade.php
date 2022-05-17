f96921e6c016746a00c61b57e0f6715be38aed51
cbbomhay2000 <canhdong1999@gmail.com>
2022-05-17 17:36:33 +0700

    add

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -13,10 +13,9 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- bootstrap-css -->
-<link href="css/admin/bootstrap.min.css" rel="stylesheet">
-
-<link href="{{ 'css/style.css' }}" rel="stylesheet" type='text/css' />
-<link href="{{ 'css/style-responsive.css' }}" rel='stylesheet'/>
+<link href="{{ asset('css/bootstrapmin.css')}}" rel="stylesheet">
+<link href="{{asset('css/style.css')}}" rel="stylesheet" type='text/css' />
+<link href="{{ asset('css/style-responsive.css')}}" rel='stylesheet'/>
 <!-- font CSS -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 <!-- font-awesome icons -->
