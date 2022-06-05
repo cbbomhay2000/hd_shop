19dfe3bf90802208285e1ab12b85e1fde334306d
cbbomhay2000 <canhdong1999@gmail.com>
2022-06-06 03:53:21 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -31,37 +31,40 @@ Route::get('/admin/login', function () {
 
 Route::get('user/profile', [App\Http\Controllers\UserProfileController::class, 'edit'])->name('profile');
 Route::put('user/profile', [App\Http\Controllers\UserProfileController::class, 'update'])->name('update-profile');
-Route::get('upload', [UploadController::class, 'index']);
 Route::post('crop', [App\Http\Controllers\UploadController::class, 'crop'])->name('crop');
 
-    Route::prefix('admin')->name('admin.')->group(function () {
-        Route::namespace('Auth')->middleware('guest:admin')->group(function () {
-            //login route
-            Route::get('/login', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'create'])->name('login');
-            Route::post('/adminlogin', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'store'])->name('adminlogin');
-        });
-        Route::get('/destroy', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'destroy'])->name('destroy');
-        Route::get('/index', [App\Http\Controllers\Admin\HomeAdminController::class, 'index'])->name('index');
-        Route::resources([
-            'admin-account' => 'App\Http\Controllers\Admin\AdminController',
-        ]);
-        Route::get('admin-account/{admin_account}', [App\Http\Controllers\Admin\AdminController::class, 'block'])->name('block');
-
-        Route::resources([
-            'user-account' => 'App\Http\Controllers\Admin\UserController',
-        ]);
-        Route::get('user-account/{user_account}', [App\Http\Controllers\Admin\UserController::class, 'block'])->name('block_user');
-
-        Route::resources([
-            'brands' => 'App\Http\Controllers\Admin\BrandController',
-        ]);
-        Route::resources([
-            'category' => 'App\Http\Controllers\Admin\CategoryController',
-        ]);
-        Route::resources([
-            'product' => 'App\Http\Controllers\Admin\ProductController',
-        ]);
-        Route::resources([
-            'product_status' => 'App\Http\Controllers\Admin\ProductStatusController',
-        ]);
+Route::prefix('admin')->name('admin.')->group(function () {
+Route::post('image', [App\Http\Controllers\Admin\UserController::class, 'image'])->name('image');
+    Route::namespace('Auth')->middleware('guest:admin')->group(function () {
+        //login route
+        Route::get('/login', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'create'])->name('login');
+        Route::post('/adminlogin', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'store'])->name('adminlogin');
     });
+    Route::get('/destroy', [App\Http\Controllers\Admin\Auth\AuthenticateSessionController::class, 'destroy'])->name('destroy');
+    Route::get('/index', [App\Http\Controllers\Admin\HomeAdminController::class, 'index'])->name('index');
+    Route::resources([
+        'admin-account' => 'App\Http\Controllers\Admin\AdminController',
+    ]);
+    Route::get('admin-account/{admin_account}', [App\Http\Controllers\Admin\AdminController::class, 'block'])->name('block');
+
+    Route::resources([
+        'user-account' => 'App\Http\Controllers\Admin\UserController',
+    ]);
+    Route::post('user-account/{user_account}', [App\Http\Controllers\Admin\UserController::class, 'image'])->name('image');
+
+    Route::get('user-account/{user_account}', [App\Http\Controllers\Admin\UserController::class, 'block'])->name('block_user');
+    Route::get('user-accounts/{user_account}', [App\Http\Controllers\Admin\UserController::class, 'show'])->name('show');
+
+    Route::resources([
+        'brands' => 'App\Http\Controllers\Admin\BrandController',
+    ]);
+    Route::resources([
+        'category' => 'App\Http\Controllers\Admin\CategoryController',
+    ]);
+    Route::resources([
+        'product' => 'App\Http\Controllers\Admin\ProductController',
+    ]);
+    Route::resources([
+        'product_status' => 'App\Http\Controllers\Admin\ProductStatusController',
+    ]);
+});
